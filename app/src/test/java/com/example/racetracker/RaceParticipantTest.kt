e75b46114846d629e64a98d7f51708c427c06f3b
smh10 <smh10.mh@gmail.com>
2024-12-19 17:38:13 -0500

    Unit Tests are written to test coroutines.

--- app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
+++ app/src/test/java/com/example/racetracker/RaceParticipantTest.kt
@@ -16,7 +16,16 @@
 package com.example.racetracker
 
 import com.example.racetracker.ui.RaceParticipant
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runCurrent
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class RaceParticipantTest {
     private val raceParticipant = RaceParticipant(
         name = "Test",
@@ -25,4 +34,54 @@ class RaceParticipantTest {
         initialProgress = 0,
         progressIncrement = 1
     )
+
+    @Test
+    fun raceParticipant_RaceStarted_ProgressUpdated() = runTest {
+        val expectedProgress = 1
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.progressDelayMillis)
+        runCurrent()
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RaceFinished_ProgressUpdated() = runTest {
+        val expectedProgress = 100
+        launch { raceParticipant.run() }
+        advanceTimeBy(raceParticipant.progressDelayMillis * raceParticipant.maxProgress)
+        runCurrent()
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RacePaused_ProgressUpdated() = runTest {
+        val expectedProgress = 10
+        val job = launch { raceParticipant.run() }
+        advanceTimeBy(expectedProgress * raceParticipant.progressDelayMillis)
+        runCurrent()
+        job.cancelAndJoin()
+        assertEquals(expectedProgress, raceParticipant.currentProgress)
+    }
+
+    @Test
+    fun raceParticipant_RacePausedAndResumed_ProgressUpdated() = runTest {
+        val expectedProgress = 10
+        repeat(2){
+            val job = launch { raceParticipant.run() }
+            advanceTimeBy(expectedProgress * raceParticipant.progressDelayMillis)
+            runCurrent()
+            job.cancelAndJoin()
+        }
+        assertEquals(expectedProgress * 2, raceParticipant.currentProgress)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun raceParticipant_ProgressIncrementZero_ExceptionThrown() = runTest {
+        RaceParticipant(name = "test", progressIncrement = 0)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun raceParticipant_MaxProgressZero_ExceptionThrown() = runTest {
+        RaceParticipant(name = "test", maxProgress = 0)
+    }
 }
